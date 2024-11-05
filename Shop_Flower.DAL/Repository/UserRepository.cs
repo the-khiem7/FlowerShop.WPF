164f8ef5ca9532e3b46cc50ed7765cc7427c7acf
TheKhiem7 <khiemnvdse180168@fpt.edu.vn>
2024-11-06 01:36:20 +0700

    add User Repository

---
+++ Shop_Flower.DAL/Repository/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Shop_Flower.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Flower.DAL.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly List<User> _users = new List<User>();
+
+        public User GetUserById(int id)
+        {
+            return _users.FirstOrDefault(u => u.UserId == id);
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _users;
+        }
+
+        public void AddUser(User user)
+        {
+            _users.Add(user);
+        }
+
+        public void UpdateUser(User user)
+        {
+            var existingUser = GetUserById(user.UserId);
+            if (existingUser != null)
+            {
+                existingUser.Username = user.Username;
+                existingUser.Password = user.Password;
+                existingUser.Email = user.Email;
+                existingUser.Role = user.Role;
+            }
+        }
+
+        public void DeleteUser(int id)
+        {
+            var user = GetUserById(id);
+            if (user != null)
+            {
+                _users.Remove(user);
+            }
+        }
+    }
+}
