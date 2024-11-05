1982da7c3f669e5c72c434929da3c8634c2b2181
TheKhiem7 <khiemnvdse180168@fpt.edu.vn>
2024-11-06 01:36:40 +0700

    Add User Services

---
+++ Shop_Flower.BLL/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Shop_Flower.DAL.Entities;
+using Shop_Flower.DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Flower.BLL
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        public User GetUserById(int id)
+        {
+            return _userRepository.GetUserById(id);
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _userRepository.GetAllUsers();
+        }
+
+        public void AddUser(User user)
+        {
+            _userRepository.AddUser(user);
+        }
+
+        public void UpdateUser(User user)
+        {
+            _userRepository.UpdateUser(user);
+        }
+
+        public void DeleteUser(int id)
+        {
+            _userRepository.DeleteUser(id);
+        }
+    }
+}
