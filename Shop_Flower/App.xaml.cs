c216cf7b6a3fe47136b956cf320770200c41e979
hien <hiennpqse180072@fpt.edu.vn>
2024-11-08 20:56:29 +0700

    WPF admin placeholder

--- Shop_Flower/App.xaml.cs
+++ Shop_Flower/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
-
+using Shop_Flower.BLL.Services;
+using Shop_Flower.DAL;
+using Shop_Flower.DAL.Repository;
 namespace Shop_Flower
 {
     /// <summary>
@@ -9,6 +11,23 @@ namespace Shop_Flower
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Create necessary dependencies
+            var context = new ShopContext();
+            var flowerInfoRepository = new FlowerInfoRepository(context);
+            var flowerInfoService = new FlowerInfoService(flowerInfoRepository);
+
+            // Create and configure AdminWindow
+            var adminWindow = new AdminWindow();
+            adminWindow.SetFlowerInfoService(flowerInfoService);
+
+            // Show the window
+            adminWindow.Show();
+        }
     }
+
 
 }
